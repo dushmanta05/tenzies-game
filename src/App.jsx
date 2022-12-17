611d71ef204f0bc60c16f4a540df0e401d0e69dd
Diilu Behera <dushmanta0511@gmail.com>
2022-12-17 23:50:00 +0530

    adding roll dice function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
-
+import { nanoid } from 'nanoid';
 
 
 import Die from './components/Die'
@@ -9,19 +9,33 @@ const App = () => {
 
   const [diceValue, setDiceValue] = React.useState(allNewDice());
 
+  function generateNewDie(){
+      return {value: Math.ceil(Math.random() * 6), 
+      isHeld: false,
+      id: nanoid()}
+  }
   function allNewDice(){
     let array = [];
     for (let index = 0; index < 10; index++) {
-      const random = Math.ceil(Math.random() * 6)
-      array.push(random)
+      array.push(generateNewDie())
     }
     return array;
   }
+  function rollDice(){
+    setDiceValue(oldDiceValue => oldDiceValue.map(obj => obj.isHeld ? obj : generateNewDie()))
+  }
+  function holdDice(id){
+    setDiceValue(oldDiceValue => oldDiceValue.map(obj => obj.id === id ? {...obj, isHeld: !obj.isHeld} : obj))
+  }
+
+  const diceElements = diceValue.map(obj => <Die isHeld={obj.isHeld} key={obj.id} value={obj.value} holdDice={() => holdDice(obj.id)} />)
+
   return (
     <main>
       <div className='dice-container'>
-       {diceValue.map(num => <Die value={num}/>)}
+       {diceElements}
       </div>
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
   )
 }
